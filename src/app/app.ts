997e3fd0e9719f4003343399ad7e424cd1854737
Viswa Prakash Kothapalli <viswaprakashk@gmail.com>
2025-06-04 10:17:29 +0530

    First Upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,signal, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  // imports: [RouterOutlet],
+  template: `<b>First Signal: {{firstSignal()}}, Scond Signal: {{secondSignal()}}</b><br/>
+            <button (click) = "handleFirstClick()">Increase First</button>
+            <button (click) = "handleSecondClick()">Increase Second</button>`,
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'angular';
+
+  firstSignal = signal(0);
+  secondSignal = signal(10);
+  handleFirstClick = () => {
+    this.firstSignal.update(prev => prev+1);
+  }
+
+  handleSecondClick = () => {
+    this.secondSignal.update(prev => prev+1);
+  }
+
+  constructor() {
+    effect( () => {
+      console.log("First Signal is changed to " + this.firstSignal());
+    });
+
+    effect ( () => {
+      console.log("Second Signal is changed to " + this.secondSignal());
+    });
+  }
+
+
 }
