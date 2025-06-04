7d795e515b0d28509d936d1b701f916c1f0d7583
Viswa Prakash Kothapalli <viswaprakashk@gmail.com>
2025-06-04 11:20:32 +0530

    More Changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component,signal, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,signal, effect, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+
+interface User {
+  id: number,
+  name: string,
+  email: string,
+  username: string
+}
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
-  template: `<b>First Signal: {{firstSignal()}}, Scond Signal: {{secondSignal()}}</b><br/>
-            <button (click) = "handleFirstClick()">Increase First</button>
-            <button (click) = "handleSecondClick()">Increase Second</button>`,
+  imports: [CommonModule],
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
@@ -14,22 +21,49 @@ export class App {
 
   firstSignal = signal(0);
   secondSignal = signal(10);
+  firstComputed = computed(() => this.firstSignal()*10);
+
+  response = signal<User[]>([]);
+
+  secondComputed = computed( () => {
+    if (this.secondSignal() < 18) {
+      return "secondSignal is less than 18: " + this.secondSignal();
+    }
+    else{
+      return "secondSignal is major";
+    }
+  });
   handleFirstClick = () => {
-    this.firstSignal.update(prev => prev+1);
+    this.firstSignal.set(this.firstSignal()+1);
   }
 
   handleSecondClick = () => {
     this.secondSignal.update(prev => prev+1);
   }
 
+  
   constructor() {
-    effect( () => {
+    effect( (onCleanup) => {
       console.log("First Signal is changed to " + this.firstSignal());
+
+      onCleanup(() => {
+        //if we have any socket.io functionalities in the applications, we can disconnect them from the backend using this onCleanup function
+      });
     });
 
     effect ( () => {
       console.log("Second Signal is changed to " + this.secondSignal());
     });
+  }
+  async fetchUserData () {
+    try{
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data =await res.json();
+      this.response.set(data);
+      console.log(this.response());
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 
